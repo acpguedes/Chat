3cbd5c03c0ea334ef82b63a774306b846d31f27a
acpguedes <guedes_1000@hotmail.com>
2012-07-06 12:16:15 -0300

    Update master

--- teste.pl
+++ teste.pl
@@ -2,22 +2,5 @@
 
 use InterfaceSB;
 
-my $if = InterfaceSB;
+my $interface = InterfaceSB;
 
-__END__
-my $work = 1;
-my @old;
-my $num = 0;
-while ($work == 1){
-	my $msg = $p->get_msg;
-	if ($num == 0){
-		$if->update_me($msg);
-		push (@old, $msg);
-		$num++;
-	}
-	elsif($old[$num - 1] ne $msg){
-		$if->update_me($msg);
-		push (@old, $msg);
-		$num++;
-	}
-}
